683da41ea23c461e57816be473095e0d966b9354
lokk344 <lokk344@gmail.com>
2026-05-07 22:35:05 +0700

    s

--- obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b6679dc71def72e25de599fc7a65d2053df21dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf47d01f562ed871d029bd6659848e06acb00336")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
